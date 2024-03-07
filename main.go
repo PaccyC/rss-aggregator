4c2e0eb95c3d5b4b3e8134b0ed2622b41faa6c3b
PaccyC <abayisengaaimepacifique@gmail.com>
2024-03-07 22:01:48 +0200

    added routing

--- main.go
+++ main.go
@@ -1,6 +1,43 @@
 package main
-import "fmt"
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
+
+	"github.com/joho/godotenv"
+)
 
 func main(){
+	godotenv.Load(".env")
 	fmt.Println("Hello world!")
+	portString := os.Getenv("PORT")
+
+	if portString == ""{
+		log.Fatal("PORT is not found in the environment")
+	}
+ fmt.Println("port: ",portString)
+
+ router:= chi.NewRouter()
+ router.Use(cors.Handler(cors.Options{
+	AllowedOrigins: []string{"https://*","http://*"},
+	AllowedMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+	AllowedHeaders: []string{"*"},
+	ExposedHeaders: []string{"Link"},
+	AllowCredentials: false,
+	MaxAge: 300,
+ }))
+ srv:= &http.Server{
+	Handler: router,
+	Addr: ":"+portString,
+ }
+ fmt.Printf("Server listening on port %v\n",portString)
+ err := srv.ListenAndServe()
+ if err != nil{
+	log.Fatal(err)
+ }
 }
