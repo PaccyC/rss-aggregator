86cba348263c225ec0b7792345d668f3cadec910
PaccyC <abayisengaaimepacifique@gmail.com>
2024-03-15 10:15:27 +0200

    added fetching posts for user

--- main.go
+++ main.go
@@ -79,6 +79,8 @@ func main(){
  v1Router.Get("/users",apiCfg.middlewareAuth(apiCfg.handlerGetUser))
  v1Router.Post("/feeds",apiCfg.middlewareAuth(apiCfg.handlerCreateFeed))
  v1Router.Get("/feeds",apiCfg.handlerGetFeeds)
+v1Router.Get("/posts",apiCfg.middlewareAuth(apiCfg.handlerGetPostsForUser))
+
  v1Router.Post("/feed-follows",apiCfg.middlewareAuth(apiCfg.handlerCreateFeedFollow))
  v1Router.Get("/feed-follows",apiCfg.middlewareAuth(apiCfg.handlerGetFeedFollows))
  v1Router.Delete("/feed-follows/{feedFollowID}",apiCfg.middlewareAuth(apiCfg.handlerDeleteFeedFollows))
